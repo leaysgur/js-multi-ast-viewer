bb3fda4d2cfe5022750e621838c69b71835212b9
Yuji Sugiura <y.sugiura.0316@gmail.com>
2025-05-16 15:41:42 +0900

    Refactor ast-view

--- src/components/ast-view.svelte
+++ src/components/ast-view.svelte
@@ -1,43 +1,82 @@
 <script lang="ts">
   type Props = { root: any };
   let { root }: Props = $props();
+
+  const isObject = (value: any): value is object =>
+    Object.prototype.toString.call(value) === "[object Object]";
+  const isRange = (value: any): value is [number, number] =>
+    Array.isArray(value) && value.length === 2 && value.every((v) => typeof v === "number");
+  const isLoc = (
+    value: any,
+  ): value is { start: { line: number; column: number }; end: { line: number; column: number } } =>
+    // prettier-ignore
+    isObject(value) && "start" in value && "end" in value &&
+    isObject(value.start) && "line" in value.start && "column" in value.start &&
+    isObject(value.end) && "line" in value.end && "column" in value.end &&
+    [value.start.line, value.start.column, value.end.line, value.end.column].every(
+      (v) => typeof v === "number",
+    );
 </script>
 
 <div>{@render view(root)}</div>
 
 {#snippet view(node: any)}
-  {#if Object.prototype.toString.call(node) === "[object Object]"}
+  {#if isObject(node)}
     <span>{"{"}</span>
     {#each Object.entries(node) as [key, value]}
       <div class="pl-4">
         {@render nodeKey(key)}<span>:</span>
+
+        <!-- Special handling for specific keys -->
         {#if key === "type"}
           {@render typeLeaf(value)}<span>,</span>
-        {:else if key === "range" && Array.isArray(value)}
+        {:else if key === "range" && isRange(value)}
           <span>[</span>
           {@render leaf(value[0])}<span>,</span>
           {@render leaf(value[1])}
           <span>]</span>
+        {:else if key === "loc" && isLoc(value)}
+          <span>{"{"}</span>
+          <div class="pl-4">
+            {@render nodeKey("start")}<span>:</span>
+            <span>{"{"}</span>
+            {@render nodeKey("line")}<span>:</span>
+            {@render leaf(value.start.line)}<span>,</span>
+            {@render nodeKey("column")}<span>:</span>
+            {@render leaf(value.start.column)}
+            <span>{"}"}</span><span>,</span>
+          </div>
+          <div class="pl-4">
+            {@render nodeKey("end")}<span>:</span>
+            <span>{"{"}</span>
+            {@render nodeKey("line")}<span>:</span>
+            {@render leaf(value.end.line)}<span>,</span>
+            {@render nodeKey("column")}<span>:</span>
+            {@render leaf(value.end.column)}
+            <span>{"}"}</span><span>,</span>
+          </div>
+          <span>{"}"}</span><span>,</span>
         {:else if Array.isArray(value)}
           {#if value.length === 0}
             <span>[</span><span>]</span><span>,</span>
           {:else}
+            <!-- Handle brackets to NOT break between key and `[` -->
             <span>[</span>
-            {@render view(value)}
+            {#each value as item}
+              <div class="pl-4">{@render view(item)}</div>
+            {/each}
             <span>]</span><span>,</span>
           {/if}
-        {:else}
+        {:else if isObject(value)}
           {@render view(value)}
+        {:else}
+          {@render leaf(value)}<span>,</span>
         {/if}
       </div>
     {/each}
     <span>{"}"}</span><span>,</span>
-  {:else if Array.isArray(node)}
-    {#each node as item}
-      <div class="pl-4">{@render view(item)}</div>
-    {/each}
   {:else}
-    {@render leaf(node)}<span>,</span>
+    {console.error("Unreachable: AST root should be Object!", node)}
   {/if}
 {/snippet}
 
@@ -63,6 +102,6 @@
   {:else if v === null}
     <span class="text-gray-400">null</span>
   {:else}
-    {console.error("Unknown type:", v)}
+    {console.error("Unreachable: Unknown leaf value", v)}
   {/if}
 {/snippet}
